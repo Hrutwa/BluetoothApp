dc45ed0e4398814dfda4d778c2b9325b8d9b1a04
Hrutwa <89697831+Hrutwa@users.noreply.github.com>
2023-05-02 10:14:56 +0530

    FIrst Commit

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,20 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAn8cm4IehrfmJJaOeqh0xhwj5iGvq2pfs",
-  authDomain: "bluetoothapp-4879b.firebaseapp.com",
-  projectId: "bluetoothapp-4879b",
-  storageBucket: "bluetoothapp-4879b.appspot.com",
-  messagingSenderId: "320935716700",
-  appId: "1:320935716700:web:e3eea835825af817214399",
-  measurementId: "G-0NTTF2704X",
+  apiKey: "AIzaSyAUaWx626F3W5n7siHNXkVb4sIMzn9Ua5Y",
+  authDomain: "bluetoothapp-74eda.firebaseapp.com",
+  projectId: "bluetoothapp-74eda",
+  storageBucket: "bluetoothapp-74eda.appspot.com",
+  messagingSenderId: "226736747920",
+  appId: "1:226736747920:web:88b6eef2cf6ed3f909ef15"
 };
 
 // Initialize Firebase
