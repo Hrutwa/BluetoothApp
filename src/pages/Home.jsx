b3d5bb37665a406a800628bc50b0d975156ffd8a
atharvamandrekarwork <atharvamandrekarwork@gmail.com>
2023-05-14 17:02:35 +0530

    Improved the UI/UX of the app

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -109,32 +109,70 @@ function Home() {
   return (
     <>
       <div>
-        {User.authUser && <button onClick={User.userSignOut}>Sign Out</button>}
-      </div>
-      <button className="btn" onClick={connectClick}>
-        Connect
+      {User.authUser && (
+        <button 
+          className="btn" 
+          style={{ 
+          marginBottom: "0px", 
+          color: "#A97142", 
+          width: "120px",
+      }} 
+      onClick={User.userSignOut}
+      >
+      <span class="material-icons" style={{ verticalAlign: "middle" }}>logout</span>
+      <span style={{ verticalAlign: "middle" }}>Sign Out</span>
       </button>
+      )}
+      </div>
+      <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", height: "110vh" }}>
+      <button className="btn" style={{ marginBottom: "0px", color: "#4EE2EC", width: "200px" }} value={0} onClick={connectClick}>
+          <span class="material-icons" style={{ verticalAlign: "middle" }}>bluetooth</span>
+          <span style={{ verticalAlign: "middle" }}>Connect Bluetooth</span>
+        </button>
+        <br />
+        {deviceCache && (
+          <button className="btn" style={{ marginBottom: "0px", color: "#4E8975", width: "200px" }} value={0} onClick={disconnectClick}>
+          <span class="material-icons" style={{ verticalAlign: "middle" }}>bluetooth_disabled</span>
+          <span style={{ verticalAlign: "middle" }}>Disconnect Bluetooth</span>
+        </button>
+        )}
+        <br />
+        {deviceCache && (
+          <button className="btn" style={{ marginBottom: "0px", color: "green", width: "200px" }} value={1} onClick={handleClick}>
+          <span class="material-icons" style={{ verticalAlign: "middle" }}>power_settings_new</span>
+          <span style={{ verticalAlign: "middle" }}>Ignition On</span>
+        </button>
+        )}
+        <br />
+        {deviceCache && (
+          <button className="btn" style={{ marginBottom: "0px", color: "orange", width: "200px" }} value={2} onClick={handleClick}>
+          <span class="material-icons" style={{ verticalAlign: "middle" }}>settings_suggest</span>
+          <span style={{ verticalAlign: "middle" }}>Start Engine</span>
+        </button>
+        )}
+        <br />
+        {deviceCache && (
+          <button className="btn" style={{ marginBottom: "0px", color: "red", width: "200px" }} value={0} onClick={handleClick}>
+            <span class="material-icons" style={{ verticalAlign: "middle" }}>power_settings_new</span>
+            <span style={{ verticalAlign: "middle" }}>Ignition Off</span>
+          </button>
+        )}
+        <br />
+        {deviceCache && (
+        <button className="btn" style={{ marginBottom: "0px", color: "#E2F516", width: "200px" }} value={3} onClick={handleClick}>
+        <span class="material-icons" style={{ verticalAlign: "middle" }}>notifications_active</span>
+        <span style={{ verticalAlign: "middle" }}>Activate Sentry Mode</span>
+        </button>
+        )}
+        <br />
+        {deviceCache && (
+        <button className="btn" style={{ marginBottom: "0px", color: "#808000", width: "200px" }} value={4} onClick={handleClick}>
+        <span class="material-icons" style={{ verticalAlign: "middle" }}>notifications_off</span>
+        <span style={{ verticalAlign: "middle" }}>Deativate Sentry Mode</span>
+        </button>
+        )}
       <br />
-      {deviceCache && (
-        <button className="btn" onClick={disconnectClick}>
-          Disconnect
-        </button>
-      )}
-      {deviceCache && (
-        <button className="btn" value={1} onClick={handleClick}>
-          Turn on Bike
-        </button>
-      )}
-      {deviceCache && (
-        <button className="btn" value={2} onClick={handleClick}>
-          Start Engine
-        </button>
-      )}
-      {deviceCache && (
-        <button className="btn" value={0} onClick={handleClick}>
-          Turn Off Engine
-        </button>
-      )}
+      </div>
     </>
   );
 }
